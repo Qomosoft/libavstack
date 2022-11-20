a57d9c8393fa6db96704f5acdf59d52d5d24cee0
Qomosoft <qomosoft@gmail.com>
2022-11-21 00:06:26 +0800

    feat:[algorithm] add solution for 0059-spiral-matrix-ii

--- algorithm/array/0059-spiral-matrix-ii.cpp
+++ algorithm/array/0059-spiral-matrix-ii.cpp
@@ -26,12 +26,56 @@
 //
 // Related Topics 数组 矩阵 模拟 👍 858 👎 0
 
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ans(n, vector<int>(n, 0));
+        int top = 0, bottom = n - 1, left = 0, right = n - 1, num = 1;
 
+        while (num <= n * n) {
+            for (int i = left; i <= right; ++i, ++num) ans[top][i] = num;
+
+            ++top;
+            for (int i = top; i <= bottom; ++i, ++num) ans[i][right] = num;
+
+            --right;
+            for (int i = right; i >= left; --i, ++num) ans[bottom][i] = num;
+
+            --bottom;
+            for (int i = bottom; i >= top; --i, ++num) ans[i][left] = num;
+
+            ++left;
+        }
+
+        return ans;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+template<typename T>
+ostream &operator<<(ostream &out, const vector<T> &v) {
+    out << '[';
+    copy(v.begin(), v.end(), ostream_iterator<T>(out, ","));
+    out << "\b]";
+
+    return out;
+}
+
+int main(int argc, const char *argv[])
+{
+    Solution solution;
+    vector<vector<int>> ans = solution.generateMatrix(3);
+    for (auto &v : ans) {
+        cout << v << endl;
+    }
+
+    return 0;
+}
