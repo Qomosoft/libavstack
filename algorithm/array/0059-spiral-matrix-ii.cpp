460a816c5da344a65740cfdff557a293b60de5b7
Robin Chou <robin.m.chou@gmail.com>
2022-11-25 19:26:24 +0800

    perf:[algorithm] 0059-spiral-matrix-ii

--- algorithm/array/0059-spiral-matrix-ii.cpp
+++ algorithm/array/0059-spiral-matrix-ii.cpp
@@ -38,20 +38,13 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> ans(n, vector<int>(n, 0));
-        int top = 0, bottom = n - 1, left = 0, right = n - 1, num = 1;
+        int top = 0, left = 0, right = n - 1, bottom = n - 1, k = 1;
 
-        while (num <= n * n) {
-            for (int i = left; i <= right; ++i, ++num) ans[top][i] = num;
-
-            ++top;
-            for (int i = top; i <= bottom; ++i, ++num) ans[i][right] = num;
-
-            --right;
-            for (int i = right; i >= left; --i, ++num) ans[bottom][i] = num;
-
-            --bottom;
-            for (int i = bottom; i >= top; --i, ++num) ans[i][left] = num;
-
+        while (k <= n * n) {
+            for (int i = left; i <= right; ++i) ans[top][i] = k++;
+            for (int i = ++top; i <= bottom; ++i) ans[i][right] = k++;
+            for (int i = --right; i >= left; --i) ans[bottom][i] = k++;
+            for (int i = --bottom; i >= top; --i) ans[i][left] = k++;
             ++left;
         }
 
