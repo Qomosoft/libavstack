826409b82b9619113f3d96dc47855c9be57ba23c
Qomosoft <qomosoft@gmail.com>
2022-12-20 00:10:46 +0800

    feat:[algorithm] add solution for 0383-ransom-note

---
+++ algorithm/hashtable/0383-ransom-note.cpp
@@ -0,0 +1,71 @@
+//给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+//
+// 如果可以，返回 true ；否则返回 false 。
+//
+// magazine 中的每个字符只能在 ransomNote 中使用一次。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：ransomNote = "a", magazine = "b"
+//输出：false
+//
+//
+// 示例 2：
+//
+//
+//输入：ransomNote = "aa", magazine = "ab"
+//输出：false
+//
+//
+// 示例 3：
+//
+//
+//输入：ransomNote = "aa", magazine = "aab"
+//输出：true
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= ransomNote.length, magazine.length <= 10⁵
+// ransomNote 和 magazine 由小写英文字母组成
+//
+//
+// Related Topics 哈希表 字符串 计数 👍 601 👎 0
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> m;
+
+        for (char c : magazine) m[c]++;
+
+        for (char c : ransomNote) {
+            m[c]--;
+            if (m[c] < 0) return false;
+        }
+
+        return true;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+int main(int argc, const char *argv[])
+{
+    string ransomNote = "aa", magazine = "aab";
+    Solution solution;
+    cout << solution.canConstruct(ransomNote, magazine) << endl;
+    return 0;
+}
