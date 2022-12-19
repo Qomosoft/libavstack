b20706532719ba362a226bcd99767092f0a5c51e
Qomosoft <qomosoft@gmail.com>
2022-12-19 23:46:53 +0800

    feat:[algorithm] add solution for 0454-4sum-ii

---
+++ algorithm/hashtable/0454-4sum-ii.cpp
@@ -0,0 +1,83 @@
+//给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+//
+//
+// 0 <= i, j, k, l < n
+// nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+//输出：2
+//解释：
+//两个元组如下：
+//1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1)
+// + 2 = 0
+//2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1)
+// + 0 = 0
+//
+//
+// 示例 2：
+//
+//
+//输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// n == nums1.length
+// n == nums2.length
+// n == nums3.length
+// n == nums4.length
+// 1 <= n <= 200
+// -2²⁸ <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2²⁸
+//
+//
+// Related Topics 数组 哈希表 👍 714 👎 0
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int> m;
+
+        for (auto &it1 : nums1) {
+            for (auto &it2 : nums2) {
+                ++m[it1 + it2];
+            }
+        }
+
+        int ans = 0;
+        for (auto &it3 : nums3) {
+            for (auto &it4 : nums4) {
+                if (m.count(-it3 - it4)) {
+                    ans += m[-it3 - it4];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+int main(int argc, const char *argv[])
+{
+    vector<int> nums1 = {1, 2}, nums2 = {-2, -1}, nums3 = {-1, 2}, nums4 = {0, 2};
+    Solution solution;
+    cout << solution.fourSumCount(nums1, nums2, nums3, nums4) << endl;
+    return 0;
+}
