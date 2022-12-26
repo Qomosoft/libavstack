f148ed9a7f4e34a7f02011bb92c759c7b2c6cdc3
robin <robin.m.chou@gmail.com>
2022-12-26 23:55:04 +0800

    perf:[algorithm] update 0541-reverse-string-ii

--- algorithm/string/0541-reverse-string-ii.cpp
+++ algorithm/string/0541-reverse-string-ii.cpp
@@ -42,14 +42,9 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         int n = s.size();
-
-        int start = 0;
-        while (start < n) {
-            int end = min(start + k, n);
-            reverse(s.begin() + start, s.begin() + end);
-            start = end + k;
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
         }
-
         return s;
     }
 };
