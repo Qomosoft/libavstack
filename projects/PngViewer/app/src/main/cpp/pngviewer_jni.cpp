91f0bf7e41a446ac5b7bac8386a79a60997aac43
Robin Chou <robin.m.chou@gmail.com>
2023-01-06 13:49:31 +0800

    feat:[pngviewer] add librenderer and libcommon

--- projects/PngViewer/app/src/main/cpp/pngviewer_jni.cpp
+++ projects/PngViewer/app/src/main/cpp/pngviewer_jni.cpp
@@ -1,44 +1,57 @@
-#include <jni.h>
+#include "pic_preview_controller.h"
 
-// Write C++ code here.
-//
-// Do not forget to dynamically load the C++ library into your application.
-//
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("pngviewer");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("pngviewer")
-//      }
-//    }
+#include <jni.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+
+#define LOG_TAG "PngViewer"
+
+static ANativeWindow *window = nullptr;
+static PicPreviewController *controller = nullptr;
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_qomo_pngviewer_PngViewer_init(JNIEnv *env, jobject thiz, jstring pic_path) {
-    // TODO: implement init()
+    controller = new PicPreviewController();
+    char *png_path = const_cast<char*>(env->GetStringUTFChars(pic_path, nullptr));
+    controller->start(png_path);
+    env->ReleaseStringUTFChars(pic_path, png_path);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_qomo_pngviewer_PngViewer_setSurface(JNIEnv *env, jobject thiz,
                                              jobject surface) {
-    // TODO: implement setSurface()
+    if (nullptr != surface && nullptr != controller) {
+        window = ANativeWindow_fromSurface(env, surface);
+        LOGI("Get window %p", window);
+        controller->setWindow(window);
+    } else if(nullptr != window) {
+        LOGI("Releasing window");
+        ANativeWindow_release(window);
+        window = nullptr;
+    }
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_qomo_pngviewer_PngViewer_resetSize(JNIEnv *env, jobject thiz, jint width,
                                             jint height) {
-    // TODO: implement resetSize()
+    if (nullptr != controller) {
+        controller->resetSize(width, height);
+    } else {
+        LOGE("controller is null");
+    }
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_qomo_pngviewer_PngViewer_stop(JNIEnv *env, jobject thiz) {
-    // TODO: implement stop()
+    if (nullptr != controller) {
+        controller->stop();
+        delete controller;
+        controller = nullptr;
+    } else {
+        LOGE("controller is null");
+    }
 }
