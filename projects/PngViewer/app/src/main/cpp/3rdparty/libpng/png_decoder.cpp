f642741367a83c353c1e1b412c47d450750d57e3
Robin Chou <robin.m.chou@gmail.com>
2023-01-06 15:52:15 +0800

    fix:[pngviewer] signal 5 (SIGTRAP), code 1 (TRAP_BRKPT) in PngPicDecoder::openFile

--- projects/PngViewer/app/src/main/cpp/3rdparty/libpng/png_decoder.cpp
+++ projects/PngViewer/app/src/main/cpp/3rdparty/libpng/png_decoder.cpp
@@ -16,6 +16,7 @@ int PngPicDecoder::openFile(char *pngFilePath) {
 	rewind(pngFile);
 	compressedData = new byte[data_length];
 	actualSize = fread(compressedData, 1, data_length, pngFile);
+    return 0;
 }
 
 RawImageData PngPicDecoder::getRawImageData(){
