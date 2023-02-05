8b5bf8aa0b6502b919f31834da0d20a05ce9422b
robin <robin.m.chou@gmail.com>
2023-02-05 23:43:10 +0800

    feat:[algorithm] add solution for binary tree's recursive traversal

---
+++ algorithm/binarytree/0145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,79 @@
+//给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[3,2,1]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 975 👎 0
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int val, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (!root) return result_;
+
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        result_.push_back(root->val);
+        return result_;
+    }
+
+private:
+    vector<int> result_;
+};
