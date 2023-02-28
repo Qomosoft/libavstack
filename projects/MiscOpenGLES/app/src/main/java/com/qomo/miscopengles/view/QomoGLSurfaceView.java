433fdcd97ac961e8ba42b3085d220ba4d6cb0689
Qomosoft <qomosoft@gmail.com>
2023-03-01 00:10:02 +0800

    feat:[miscopengles] draw air hockey stuff

--- projects/MiscOpenGLES/app/src/main/java/com/qomo/miscopengles/view/QomoGLSurfaceView.java
+++ projects/MiscOpenGLES/app/src/main/java/com/qomo/miscopengles/view/QomoGLSurfaceView.java
@@ -1,13 +1,48 @@
 package com.qomo.miscopengles.view;
 
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+
 import android.content.Context;
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+
+import com.qomo.miscopengles.R;
+import com.qomo.miscopengles.util.ShaderHelper;
+import com.qomo.miscopengles.util.TextResourceReader;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class QomoGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Renderer {
+    private static final String TAG = "QomoGLSurfaceView";
+
+    private static final String U_COLOR = "u_Color";
+    private static final String A_POSITION = "a_Position";
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+    private int program;
+    private int uColorLocation;
+    private int aPositionLocation;
+
+
     public QomoGLSurfaceView(Context context) {
         super(context);
 
@@ -17,22 +52,104 @@ public class QomoGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Re
         setRenderer(this);
         // Render the view only when there is a change in the drawing data
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        //float[] tableVerticesWithTriangles = {
+        //    // Triangle 1
+        //    0f, 0f,
+        //    9f, 14f,
+        //    0f, 14f,
+        //
+        //    // Triangle 2
+        //    0f, 0f,
+        //    9f, 0f,
+        //    9f, 14f,
+        //
+        //    // Line 1
+        //    0f, 7f,
+        //    9f, 7f,
+        //
+        //    // Mallets
+        //    4.5f, 2f,
+        //    4.5f, 12f
+        //};
+
+        float[] tableVerticesWithTriangles = {
+            // Triangle 1
+            -0.5f, -0.5f,
+            0.5f,  0.5f,
+            -0.5f,  0.5f,
+
+            // Triangle 2
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.5f,  0.5f,
+
+            // Line 1
+            -0.5f, 0f,
+            0.5f, 0f,
+
+            // Mallets
+            0f, -0.25f,
+            0f,  0.25f
+        };
+
+        vertexData = ByteBuffer
+            .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer();
+
+        vertexData.put(tableVerticesWithTriangles);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        String vertexShaderSource = TextResourceReader
+            .readTextFileFromResource(getContext(), R.raw.simple_vertex_shader);
+        String fragmentShaderSource = TextResourceReader
+            .readTextFileFromResource(getContext(), R.raw.simple_fragment_shader);
+
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        ShaderHelper.validateProgram(program);
+
+        glUseProgram(program);
+        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
+        glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        // Redraw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        // Redraw background color.
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw the table.
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Draw the center dividing line.
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_LINES, 6, 2);
+
+        // Draw the first mallet blue.
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 8, 1);
+
+        // Draw the second mallet red.
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 9, 1);
     }
 }
