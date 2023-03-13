a44dfe337792596c9fcc8ada879e5dbed1a622a2
robin <robin.m.chou@gmail.com>
2023-03-13 23:40:21 +0800

    feat:[algorithm] add solution for 0637-average-of-levels-in-binary-tree

---
+++ algorithm/binarytree/0637-average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,80 @@
+//给定一个非空二叉树的根节点
+// root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10⁻⁵ 以内的答案可以被接受。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[3.00000,14.50000,11.00000]
+//解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+//因此返回 [3, 14.5, 11] 。
+//
+//
+// 示例 2:
+//
+//
+//
+//
+//输入：root = [3,9,20,15,7]
+//输出：[3.00000,14.50000,11.00000]
+//
+//
+//
+//
+// 提示：
+//
+//
+//
+//
+//
+// 树中节点数量在 [1, 10⁴] 范围内
+// -2³¹ <= Node.val <= 2³¹ - 1
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 396 👎 0
+
+#include "binary_tree_def.h"
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+        if (!root) return result;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            double sum  = 0;
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            result.push_back(sum / size);
+        }
+
+        return result;
+    }
+};
