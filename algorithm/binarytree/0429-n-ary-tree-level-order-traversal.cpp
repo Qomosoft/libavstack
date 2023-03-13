eecd42b569c37a8d8803c336c4c3fbfc54e3901d
robin <robin.m.chou@gmail.com>
2023-03-13 23:54:52 +0800

    feat:[algorithm] add solution for 0429-n-ary-tree-level-order-traversal

---
+++ algorithm/binarytree/0429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,100 @@
+//给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+//
+// 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：root = [1,null,3,2,4,null,5,6]
+//输出：[[1],[3,2,4],[5,6]]
+//
+//
+// 示例 2：
+//
+//
+//
+//
+//输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,
+//null,13,null,null,14]
+//输出：[[1],[2,3,4,5],[6,7,8,9,10],[11,12,13],[14]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树的高度不会超过 1000
+// 树的节点总数在 [0, 10^4] 之间
+//
+//
+// Related Topics 树 广度优先搜索 👍 350 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+    Node(int x) : val(x) {}
+    Node(int x, vector<Node*> nodes) : val(x), children(nodes) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+            for (int i = 0; i < size; ++i) {
+                Node *node = q.front();
+                q.pop();
+                level.push_back(node->val);
+
+                for (int j = 0; j < node->children.size(); ++j) {
+                    q.push(node->children[j]);
+                }
+            }
+            result.push_back(level);
+        }
+
+        return result;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
