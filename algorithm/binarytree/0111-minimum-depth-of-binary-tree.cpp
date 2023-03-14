1af89de0c93308d7ff4511672505281a9315871b
robin <robin.m.chou@gmail.com>
2023-03-14 23:50:27 +0800

    feat:[algorithm] add solution for 0111-minimum-depth-of-binary-tree

---
+++ algorithm/binarytree/0111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,73 @@
+//给定一个二叉树，找出其最小深度。
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+//
+// 说明：叶子节点是指没有子节点的节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数的范围在 [0, 10⁵] 内
+// -1000 <= Node.val <= 1000
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 952 👎 0
+
+#include "binary_tree_def.h"
+#include <queue>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        int depth = 0;
+        if (!root) return depth;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            ++depth;
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (!node->left && !node->right) return depth;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+
+        return depth;
+    }
+};
