1278eb8715ab8fe7981eecc05e2ce6979481ff82
robin <robin.m.chou@gmail.com>
2023-03-14 23:42:17 +0800

    feat:[algorithm] add solution for 0104-maximum-depth-of-binary-tree

---
+++ algorithm/binarytree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,56 @@
+//给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例： 给定二叉树 [3,9,20,null,null,15,7]，
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+// 返回它的最大深度 3 。
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1542 👎 0
+
+#include "binary_tree_def.h"
+#include <queue>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        if (!root) return depth;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            ++depth;
+        }
+
+        return depth;
+    }
+};
