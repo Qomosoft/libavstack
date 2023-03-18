977f4e9b7b056ee9c1e97d5bef0dfb50bf8ee5b7
robin <robin.m.chou@gmail.com>
2023-03-18 15:50:08 +0800

    feat:[algorithm] add solution for 0101-symmetric-tree

---
+++ algorithm/binarytree/0101-symmetric-tree.cpp
@@ -0,0 +1,61 @@
+//给你一个二叉树的根节点 root ， 检查它是否轴对称。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,2,2,null,3,null,3]
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [1, 1000] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 2326 👎 0
+
+#include "binary_tree_def.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return check(root->left, root->right);
+    }
+
+    bool check(TreeNode *left, TreeNode *right) {
+        if (left == nullptr && right != nullptr) return false;
+        else if (left != nullptr && right == nullptr) return false;
+        else if (left == nullptr && right == nullptr) return true;
+        else if (left->val != right->val) return false;
+        else return check(left->left, right->right) && check(left->right, right->left);
+    }
+};
