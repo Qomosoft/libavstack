c509d6f3ee87814194020292715e7fbd179e33a8
Qomosoft <qomosoft@gmail.com>
2023-04-10 15:52:59 +0800

    refactor:[algorithm] code style

--- algorithm/binarytree/0226-invert-binary-tree.cpp
+++ algorithm/binarytree/0226-invert-binary-tree.cpp
@@ -59,7 +59,7 @@ struct TreeNode {
     TreeNode *left;
     TreeNode *right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(left) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 class Solution {
 public:
