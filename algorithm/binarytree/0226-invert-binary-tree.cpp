3ab4c03ff95c450a40ab79a808689cff10941e24
Qomosoft <qomosoft@gmail.com>
2023-04-10 16:20:01 +0800

    feat:[algorithm] add solution for 0226-invert-binary-tree

--- algorithm/binarytree/0226-invert-binary-tree.cpp
+++ algorithm/binarytree/0226-invert-binary-tree.cpp
@@ -72,3 +72,24 @@ public:
         return root;
     }
 };
+
+class SolutionBfs {
+public:
+    TreeNode* invertTree(TreeNode *root) {
+        if (!root) return root;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode *node = q.front();
+                q.pop();
+                swap(node->left, node->right);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
