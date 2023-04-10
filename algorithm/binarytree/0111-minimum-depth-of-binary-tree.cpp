a21730aeccacdd4c31b9873e30e92de84e8674e7
Qomosoft <qomosoft@gmail.com>
2023-04-10 16:37:48 +0800

    feat:[algorithm] add solution for 0111-minimum-depth-of-binary-tree

--- algorithm/binarytree/0111-minimum-depth-of-binary-tree.cpp
+++ algorithm/binarytree/0111-minimum-depth-of-binary-tree.cpp
@@ -71,3 +71,12 @@ public:
         return depth;
     }
 };
+
+class SolutionDfs {
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+        if (!root->left) return minDepth(root->right) + 1;
+        if (!root->right) return minDepth(root->left) + 1;
+        return std::min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
