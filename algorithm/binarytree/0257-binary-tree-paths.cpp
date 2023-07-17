69b750aa0b91a1e719344a9e16113a62a2b2d648
Qomosoft <qomosoft@gmail.com>
2023-07-17 23:24:51 +0800

    feat:[algorithm] add solution I for 0257-binary-tree-paths

--- algorithm/binarytree/0257-binary-tree-paths.cpp
+++ algorithm/binarytree/0257-binary-tree-paths.cpp
@@ -79,3 +79,26 @@ public:
         }
     }
 };
+
+class SolutionI {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        string path;
+        if (!root) return result;
+        traversal(root, path, result);
+
+        return result;
+    }
+
+    void traversal(TreeNode* cur, string path, vector<string>& result) {
+        path += to_string(cur->val);
+        if (cur->left == nullptr && cur->right == nullptr) {
+            result.push_back(path);
+            return;
+        }
+
+        if (cur->left) traversal(cur->left, path + "->", result);
+        if (cur->right) traversal(cur->right, path + "->", result);
+    }
+};
