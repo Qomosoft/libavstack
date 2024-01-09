e7be77ac2ec677b558521c7fc125fea0028df1a5
Robin Chou <robin.m.chou@gmail.com>
2024-01-09 18:19:31 +0800

    feat:[algorithm] add solution for 0077-combinations

---
+++ algorithm/backtracking/0077-combinations.cpp
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=cpp
+ *
+ * [77] 组合
+ *
+ * https://leetcode.cn/problems/combinations/description/
+ *
+ * algorithms
+ * Medium (77.06%)
+ * Likes:    1568
+ * Dislikes: 0
+ * Total Accepted:    638.6K
+ * Total Submissions: 828.7K
+ * Testcase Example:  '4\n2'
+ *
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * 
+ * 你可以按 任何顺序 返回答案。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 4, k = 2
+ * 输出：
+ * [
+ * ⁠ [2,4],
+ * ⁠ [3,4],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ [1,3],
+ * ⁠ [1,4],
+ * ]
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1, k = 1
+ * 输出：[[1]]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 1 
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = startIndex; i <= n; ++i) {
+            path.push_back(i);
+            backtracking(n, k, i + 1);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return ans;
+    }
+
+private:
+    vector<int> path;
+    vector<vector<int>> ans;
+};
+// @lc code=end
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
