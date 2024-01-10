6a26a30303409b28cc8d530d01f3100359cd26a0
Robin Chou <robin.m.chou@gmail.com>
2024-01-10 18:20:44 +0800

    feat:[algorithm] add solution for 0040-combination-sum-ii

---
+++ algorithm/backtracking/0040-combination-sum-ii.cpp
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode.cn id=40 lang=cpp
+ *
+ * [40] 组合总和 II
+ *
+ * https://leetcode.cn/problems/combination-sum-ii/description/
+ *
+ * algorithms
+ * Medium (59.50%)
+ * Likes:    1504
+ * Dislikes: 0
+ * Total Accepted:    487.9K
+ * Total Submissions: 820K
+ * Testcase Example:  '[10,1,2,7,6,1,5]\n8'
+ *
+ * 给定一个候选人编号的集合 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * 
+ * candidates 中的每个数字在每个组合中只能使用 一次 。
+ * 
+ * 注意：解集不能包含重复的组合。 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+ * 输出:
+ * [
+ * [1,1,6],
+ * [1,2,5],
+ * [1,7],
+ * [2,6]
+ * ]
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: candidates = [2,5,2,1,2], target = 5,
+ * 输出:
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= candidates.length <= 100
+ * 1 <= candidates[i] <= 50
+ * 1 <= target <= 30
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, target, 0, 0);
+        return result;
+    }
+
+private:
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
+        if (target == sum) {
+            result.push_back(path);
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; ++i) {
+            if (i > startIndex && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i + 1);
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+
+    vector<int> path;
+    vector<vector<int>> result;
+};
+// @lc code=end
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
