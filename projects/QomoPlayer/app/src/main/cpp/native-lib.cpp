dee78225414b3d1b80c4da8c903c7ce25303c86d
Robin Chou <robin.m.chou@gmail.com>
2024-03-05 15:18:03 +0800

    feat:[projects] add prebuilt ffmpeg libs

--- projects/QomoPlayer/app/src/main/cpp/native-lib.cpp
+++ projects/QomoPlayer/app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,13 @@
+extern "C" {
+#include "libavutil/imgutils.h"
+#include "libswscale/swscale.h"
+#include "libavutil/dict.h"
+#include "libswresample/swresample.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/timestamp.h"
+#include "libavformat/avformat.h"
+}
+
 #include <jni.h>
 #include <string>
 
@@ -7,6 +17,7 @@ JNICALL
 Java_com_qomo_qomoplayer_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+//    std::string hello = "Hello from C++";
+    std::string hello = avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
 }
