12c67549410ba46de7887cc8407c50494426a928
Robin Chou <robin.m.chou@gmail.com>
2024-03-07 23:59:21 +0800

    feat:[algorithm] add 0718-maximum-length-of-repeated-subarray

---
+++ algorithm/dynamicprogramming/0718-maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=718 lang=cpp
+ *
+ * [718] 最长重复子数组
+ *
+ * https://leetcode.cn/problems/maximum-length-of-repeated-subarray/description/
+ *
+ * algorithms
+ * Medium (56.78%)
+ * Likes:    1059
+ * Dislikes: 0
+ * Total Accepted:    228.1K
+ * Total Submissions: 401.7K
+ * Testcase Example:  '[1,2,3,2,1]\n[3,2,1,4,7]'
+ *
+ * 给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+ * 输出：3
+ * 解释：长度最长的公共子数组是 [3,2,1] 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+ * 输出：5
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 100
+ * 
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+        int result = 0;
+        for (int i = 1; i <= nums1.size(); i++) {
+            for (int j = 1; j <= nums2.size(); j++) {
+                if (nums1[i - 1] == nums2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                result = max(result, dp[i][j]);
+            }
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
