00cf3a960051bc27eb50b93a1d3268effb2c7394
Robin Chou <robin.m.chou@gmail.com>
2024-03-08 11:24:07 +0800

    feat:[projects][qomoplayer] add QomoPlayer class

--- projects/QomoPlayer/app/src/main/cpp/qomoplayer_jni.cpp
+++ projects/QomoPlayer/app/src/main/cpp/qomoplayer_jni.cpp
@@ -5,10 +5,12 @@ extern "C" {
 #include "libswscale/swscale.h"
 #include "libswresample/swresample.h"
 #include "libavformat/avformat.h"
+#include "libavfilter/avfilter.h"
 }
 
 #include <jni.h>
 #include <string>
+#include <sstream>
 #include "logging.h"
 #include <android/log.h>
 
@@ -17,8 +19,17 @@ JNICALL
 Java_com_qomo_qomoplayer_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-//    std::string hello = "Hello from C++";
-    std::string hello = avcodec_configuration();
+    std::stringstream ss;
+    ss << "libavcodec:" << AV_STRINGIFY(LIBAVCODEC_VERSION) <<
+       "\nlibavformat:" << AV_STRINGIFY(LIBAVFORMAT_VERSION) <<
+       "\nlibavutil:" << AV_STRINGIFY(LIBAVUTIL_VERSION) <<
+       "\nlibavfilter:" << AV_STRINGIFY(LIBAVFILTER_VERSION) <<
+       "\nlibavswresample:" << AV_STRINGIFY(LIBSWRESAMPLE_VERSION) <<
+       "\nlibavswscale:" << AV_STRINGIFY(LIBSWSCALE_VERSION) <<
+       "\navcodec_configure:" << avcodec_configuration() <<
+       "\navcodec_license:" << avcodec_license();
+    std::string hello = ss.str();
+
     av_log_set_callback(ff_log_callback);
     av_log_set_level(AV_LOG_TRACE);
     av_log(nullptr, AV_LOG_DEBUG, "%s", "Log from av_log");
---
+++ projects/QomoPlayer/app/src/main/java/com/qomo/qomoplayer/mediaplayer/QomoPlayer.java
@@ -0,0 +1,46 @@
+package com.qomo.qomoplayer.mediaplayer;
+
+import android.view.Surface;
+import android.view.SurfaceHolder;
+
+public class QomoPlayer {
+    public QomoPlayer() {
+
+    }
+
+    public void setDisplay(SurfaceHolder sh) {
+
+    }
+
+    public void setSurface(Surface surface) {
+
+    }
+
+    public void setVideoScalingMode(int mode) {
+
+    }
+
+    public void setDataSource(String path) {
+
+    }
+
+    public void prepare() {
+
+    }
+
+    public void prepareAsync() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void pause() {
+
+    }
+}
